c0db46812caa536e452b0700251d5fc4feed1500
Alihaider3112 <ali3112.asif@gmail.com>
2024-07-18 14:33:54 +0500

    user_login_implementation

---
+++ src/pages/api/login/index.js
@@ -0,0 +1,29 @@
+import connect from '../../../connection/index';
+import Users from '../../../models/user';
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'myverysecuresecretkey2912'; // Replace with your actual secret key
+
+export default async function handler(req, res) {
+    await connect();
+
+    if (req.method === 'POST') {
+        const { email, password } = req.body;
+
+        try {
+            const user = await Users.findOne({email,password});
+            if (!user) {
+                return res.status(400).json({ message: 'Invalid credentials' });
+            }
+            const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '1h' });
+            res.status(200).json({token});
+        } catch (error) {
+            res.status(400).json({ message: 'Error', error });
+        }
+    }
+     else {
+        res.setHeader('Allow', ['POST', 'GET']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}
