3e89d75859e1dda342cf0c3fba851bd5824aa21d
Alihaider3112 <ali3112.asif@gmail.com>
2024-08-12 16:12:13 +0500

    fixed stuff

--- src/connection/index.js
+++ src/connection/index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 export const connect = async () => {
     try {
-         await mongoose.connect("mongodb+srv://saaadii7:epvodr1knkou4cd9@dev.p2qvlks.mongodb.net/etagpro?retryWrites=true&w=majority&appName=Dev");
+         await mongoose.connect(process.env.DATABASE_URL);
         console.log("Database connected successfully");
     } catch (error) {
         console.error('Database connection error:', error);
